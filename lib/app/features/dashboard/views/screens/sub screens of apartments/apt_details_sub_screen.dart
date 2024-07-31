57c425b1dab989bbf25eaaddd41e02950c79cf73
Musaev, Parviz <34913301+SypNeVash@users.noreply.github.com>
2024-07-31 11:41:12 +0300

    Some fix

--- lib/app/features/dashboard/views/screens/sub screens of apartments/apt_details_sub_screen.dart
+++ lib/app/features/dashboard/views/screens/sub screens of apartments/apt_details_sub_screen.dart
@@ -4,9 +4,9 @@ import 'package:apartments/app/features/dashboard/views/components/responsive_ra
 import 'package:apartments/app/models/get_all_appart_model.dart';
 import 'package:apartments/app/providers/appartment_provider.dart';
 import 'package:apartments/app/utils/services/shared_preferences.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider_plus/carousel_slider_plus.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
-import 'package:flutter/material.dart' hide CarouselController;
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
