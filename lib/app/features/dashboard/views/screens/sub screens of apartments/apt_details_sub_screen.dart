0777662461a78dec478337220186090b2be00c46
Musaev, Parviz <34913301+SypNeVash@users.noreply.github.com>
2024-07-31 11:18:44 +0300

    Update apt_details_sub_screen.dart

--- lib/app/features/dashboard/views/screens/sub screens of apartments/apt_details_sub_screen.dart
+++ lib/app/features/dashboard/views/screens/sub screens of apartments/apt_details_sub_screen.dart
@@ -6,7 +6,7 @@ import 'package:apartments/app/providers/appartment_provider.dart';
 import 'package:apartments/app/utils/services/shared_preferences.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide CarouselController;
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
